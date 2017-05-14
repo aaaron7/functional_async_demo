4d85369ecb10b908e4fd79e29d093d644f9d4995
aaaron7 <aaron.wwj@alibaba-inc.com>
2017-05-15 01:02:55 +0800

    add keynote

--- TestAsync/TestAsync/ViewController.swift
+++ TestAsync/TestAsync/ViewController.swift
@@ -7,28 +7,28 @@
 //
 
 import UIKit
-typealias AsyncFunc = (info : AnyObject,complete:(AnyObject?,NSError?)->Void) -> Void
+typealias AsyncFunc = (_ info : AnyObject,_ complete:(AnyObject?,NSError?)->Void) -> Void
 
 
 infix operator <> {associativity left precedence 150}
 infix operator +> {associativity left precedence 150}
 
 
-func +>(left : AsyncFunc , right : AsyncFunc) -> AsyncFunc{
+func +>(left : @escaping AsyncFunc , right : @escaping AsyncFunc) -> AsyncFunc{
     return { info , complete in
-        left(info: info){ result,error in
+        left(info){ result,error in
             guard error == nil else{
                 complete(nil,error)
                 return
             }
-            right(info: info){result,error in
+            right(info){result,error in
                 complete(result,error)
             }
         }
     }
 }
 
-func <>(left : AsyncFunc , right : AsyncFunc) -> AsyncFunc{
+func <>(left : @escaping AsyncFunc , right : @escaping AsyncFunc) -> AsyncFunc{
     return { info, complete in
         
         var leftComplete = false
@@ -44,14 +44,14 @@ func <>(left : AsyncFunc , right : AsyncFunc) -> AsyncFunc{
                 objc_sync_enter(finishedComplete)
                 if !finishedComplete{
                     let finalResult:[AnyObject] = [leftResult!, rightResult!]
-                    complete(finalResult, nil)
+                    complete(finalResult as AnyObject?, nil)
                     finishedComplete = true
                 }
                 objc_sync_exit(finishedComplete)
             }
         }
         
-        left(info: info){result,error in
+        left(info){result,error in
             guard error == nil else{
                 complete(nil, error)
                 return
@@ -62,7 +62,7 @@ func <>(left : AsyncFunc , right : AsyncFunc) -> AsyncFunc{
             checkComplete()
         }
         
-        right(info: info){result,error in
+        right(info){result,error in
             guard error == nil else{
                 complete(nil, error)
                 return
@@ -77,42 +77,42 @@ func <>(left : AsyncFunc , right : AsyncFunc) -> AsyncFunc{
 
 class Promise {
     var chain : AsyncFunc
-    var alwaysClosure : (Void->Void)?
-    var errorClosure : (NSError?->Void)?
+    var alwaysClosure : ((Void)->Void)?
+    var errorClosure : ((NSError?)->Void)?
     
-    init(starter : AsyncFunc){
+    init(starter : @escaping AsyncFunc){
         chain = starter
     }
     
-    func then(body : AnyObject throws->Void )->Promise{
+    func then(_ body : @escaping (AnyObject) throws->Void )->Promise{
         let async: AsyncFunc = { info, complete in
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0)) {
+            DispatchQueue.global(priority: DispatchQueue.GlobalQueuePriority.default).async {
                 var error : NSError?
                 do{
                     try body(info)
                 }catch let err as NSError{
                     error = err
                 }
-                complete(0,error)
+                complete(0 as AnyObject?,error)
             }
         }
         chain = chain +> async
         return self
     }
     
-    func always(closure : Void->Void)->Promise{
+    func always(_ closure : @escaping (Void)->Void)->Promise{
         alwaysClosure = closure
         return self
     }
     
-    func error(closure : NSError?->Void)->Promise{
+    func error(_ closure : @escaping (NSError?)->Void)->Promise{
         errorClosure = closure
         fire()
         return self
     }
     
     func fire(){
-        chain(info: 0) { (info, error) in
+        chain(0 as AnyObject) { (info, error) in
             if let always = self.alwaysClosure{
                 always()
             }
@@ -128,30 +128,30 @@ class Promise {
     }
 }
 
-func firstly(body : Void->Void)->Promise{
+func firstly(_ body : @escaping (Void)->Void)->Promise{
     
     let starter: AsyncFunc = { _,complete in
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0)) {
+        DispatchQueue.global(priority: DispatchQueue.GlobalQueuePriority.default).async {
             body();
-            complete(0,nil);
+            complete(0 as AnyObject?,nil);
         }
     }
 
     return Promise(starter: starter)
 }
 
-func when(fstBody : (Void->Void), sndBody : (Void->Void)){
+func when(_ fstBody : @escaping ((Void)->Void), sndBody : @escaping ((Void)->Void)){
     let async1 : AsyncFunc = { _ , complete in
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0)) {
+        DispatchQueue.global(priority: DispatchQueue.GlobalQueuePriority.default).async {
             fstBody();
-            complete(0,nil);
+            complete(0 as AnyObject?,nil);
         }
     }
     
     let async2 : AsyncFunc = { _ , complete in
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0)) {
+        DispatchQueue.global(priority: DispatchQueue.GlobalQueuePriority.default).async {
             sndBody();
-            complete(0,nil);
+            complete(0 as AnyObject?,nil);
         }
     }
     
@@ -159,7 +159,7 @@ func when(fstBody : (Void->Void), sndBody : (Void->Void)){
     
     var finished = false
     
-    async(info: 0) { (_, _) in
+    async(0 as AnyObject) { (_, _) in
         finished = true
     }
     
